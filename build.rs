f78e9ef9e8ca0e013467f98c53de38230179c790
Konstantin Olkhovskiy <lupus@oxnull.net>
2023-09-27 09:53:45 +0400

    Change deploy model to "Piggyback cargo vendor"
    ...and some related changes

--- build.rs
+++ build.rs
@@ -1,13 +1,3 @@
-fn main() -> std::io::Result<()> {
-    /* Generation of OCaml .ml/.mli is conditional on
-     * OCAML_BUILD_GENERATE_SIGNATURES environment variable, which is set in
-     * corresponding dune rule, launching cargo. This avoids generation when
-     * cargo is launched by IDE in source dir */
-    match std::env::var("OCAML_BUILD_GENERATE_SIGNATURES") {
-        Ok(v) => match v.as_str() {
-            "OCAML_LWT_INTEROP" => ocaml_build::Sigs::new("rust.ml").generate(),
-            _ => Result::Ok(()),
-        },
-        Err(_) => Result::Ok(()),
-    }
+pub fn main() -> std::io::Result<()> {
+    ocaml_build::Sigs::new("lib/stubs.ml").generate()
 }
--- lib/Rust_async.ml
+++ lib/Rust_async.ml
@@ -1,33 +1,33 @@
 module Promise = struct
-  type 'a t = Rust.promise
+  type 'a t = Stubs.promise
 
-  let create () = Rust.lwti_promise_create ()
+  let create () = Stubs.lwti_promise_create ()
 
   let of_lwt fut =
     let p = create () in
     Lwt.on_any
       fut
-      (fun value -> Rust.lwti_promise_resolve p value)
-      (fun exn -> Rust.lwti_promise_reject p exn);
+      (fun value -> Stubs.lwti_promise_resolve p value)
+      (fun exn -> Stubs.lwti_promise_reject p exn);
     p
   ;;
 end
 
 module Runtime = struct
   type t =
-    { executor : Rust.executor
+    { executor : Stubs.executor
     ; notification : int
     }
 
   let create () =
     let notification = Lwt_unix.make_notification ~once:false Fun.id in
-    let executor = Rust.lwti_executor_create notification in
+    let executor = Stubs.lwti_executor_create notification in
     Lwt_unix.set_notification notification (fun () ->
-      Rust.lwti_executor_run_pending executor);
+      Stubs.lwti_executor_run_pending executor);
     { executor; notification }
   ;;
 
-  let test t ~f = Rust.lwti_executor_test t.executor f
+  let test t ~f = Stubs.lwti_executor_test t.executor f
 
   let destroy t =
     Lwt_unix.stop_notification t.notification;
@@ -35,7 +35,7 @@ module Runtime = struct
   ;;
 
   module Private = struct
-    type rust_executor = Rust.executor
+    type rust_executor = Stubs.executor
 
     let rust_executor_of_t { executor; _ } = executor
   end
