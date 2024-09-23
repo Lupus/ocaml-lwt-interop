9e51e4a120a1d435bf748b6a705d441d31e520c2
Konstantin Olkhovskiy <lupus@oxnull.net>
2024-09-24 00:11:49 +0400

    Rework all the things!

--- src/notification.rs
+++ src/notification.rs
@@ -1,5 +1,3 @@
-use crate::local_executor::Notifier;
-
 extern "C" {
     pub fn lwt_unix_send_notification(id: isize);
 }
@@ -10,11 +8,5 @@ pub struct Notification(pub isize);
 impl Notification {
     pub fn send(self) {
         unsafe { lwt_unix_send_notification(self.0) }
-    }
-}
-
-impl Notifier for Notification {
-    fn notify(&self) {
-        self.send()
     }
 }
