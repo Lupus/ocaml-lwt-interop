21a3a6003075a06adfeb0d5d6de273ee90e38839
Konstantin Olkhovskiy <lupus@oxnull.net>
2024-09-24 00:11:49 +0400

    Rework all the things!

--- lib/Rust_async.ml
+++ lib/Rust_async.ml
@@ -1,18 +1,3 @@
-module Promise = struct
-  type 'a t = Stubs.promise
-
-  let create () = Stubs.lwti_promise_create ()
-
-  let of_lwt fut =
-    let p = create () in
-    Lwt.on_any
-      fut
-      (fun value -> Stubs.lwti_promise_resolve p value)
-      (fun exn -> Stubs.lwti_promise_reject p exn);
-    p
-  ;;
-end
-
 module Runtime = struct
   type t =
     { executor : Stubs.executor
@@ -24,10 +9,11 @@ module Runtime = struct
     let executor = Stubs.lwti_executor_create notification in
     Lwt_unix.set_notification notification (fun () ->
       Stubs.lwti_executor_run_pending executor);
+    Stubs.lwti_executor_run_pending executor;
     { executor; notification }
   ;;
 
-  let test t ~f = Stubs.lwti_executor_test t.executor f
+  let test t = Stubs.lwti_executor_test t.executor
 
   let destroy t =
     Lwt_unix.stop_notification t.notification;
@@ -40,3 +26,11 @@ module Runtime = struct
     let rust_executor_of_t { executor; _ } = executor
   end
 end
+
+let () =
+  Callback.register "olwti_lwt_task" Lwt.task;
+  Callback.register "olwti_lwt_wakeup_later" Lwt.wakeup_later;
+  Callback.register "olwti_lwt_wakeup_later_exn" (fun resolver msg ->
+    Lwt.wakeup_later_exn resolver (Failure msg));
+  Callback.register "olwti_printexc_to_string" Printexc.to_string
+;;
