7a98a9e65b26c79892897b43cd047dcc45ff51e8
Konstantin Olkhovskiy <lupus@oxnull.net>
2024-10-04 12:23:54 +0400

    Greatly simplify tracking of current executor
    It's a bit faster now too

--- test-stubs/src/lib.rs
+++ test-stubs/src/lib.rs
@@ -7,61 +7,49 @@ use ocaml_lwt_interop::bridged_executor::{
 use ocaml_lwt_interop::promise::Promise;
 use tokio::time::{sleep, Duration};
 
-#[ocaml::func]
-pub fn lwti_tests_bench() -> Promise<()> {
-    let (fut, resolver) = Promise::new(gc);
-    let task = spawn_using_runtime(gc, async move {
+#[ocaml_lwt_interop::func]
+#[ocaml_gen::func]
+pub fn lwti_tests_bench() -> () {
+    future::yield_now().await;
+    resolver.resolve(&ocaml_runtime(), &());
+}
+
+#[ocaml_lwt_interop::func]
+#[ocaml_gen::func]
+pub fn lwti_tests_test1() {
+    future::yield_now().await;
+    spawn(async {
         future::yield_now().await;
         resolver.resolve(&ocaml_runtime(), &());
-    });
-    task.detach();
-    fut
+    })
+    .await
 }
 
-#[ocaml::func]
-pub fn lwti_tests_test1() -> Promise<()> {
-    let (fut, resolver) = Promise::new(gc);
-    let task = spawn_using_runtime(gc, async move {
-        future::yield_now().await;
-        spawn(async {
-            future::yield_now().await;
-            resolver.resolve(&ocaml_runtime(), &());
-        })
-        .await
-    });
-    task.detach();
-    fut
-}
-
-#[ocaml::func]
-pub fn lwti_tests_test2(f: OCamlAsyncFunc<(), ()>) -> Promise<()> {
-    let (fut, resolver) = Promise::new(gc);
-    let task: Task<Result<(), String>> = spawn_using_runtime(gc, async move {
+#[ocaml_lwt_interop::func]
+#[ocaml_gen::func]
+pub fn lwti_tests_test2(f: OCamlAsyncFunc<(), ()>) -> () {
+    let task: Task<Result<(), String>> = spawn(async move {
         future::yield_now().await;
         f.call(()).await.map_err(|e| e.to_string())?;
         Ok(())
     });
-    let task = spawn_using_runtime(gc, async move {
-        let handle = bridged_executor::handle();
-        future::yield_now().await;
-        tokio::spawn(async move {
+    let handle = bridged_executor::handle();
+    future::yield_now().await;
+    tokio::spawn(async move {
+        sleep(Duration::from_secs(0)).await;
+        let task = handle.spawn(async {
+            let res = task.await;
             sleep(Duration::from_secs(0)).await;
-            let task = handle.spawn(async {
-                let res = task.await;
-                sleep(Duration::from_secs(0)).await;
-                let gc = ocaml_runtime();
-                match res {
-                    Ok(()) => {
-                        resolver.resolve(&gc, &());
-                    }
-                    Err(msg) => {
-                        resolver.reject(&gc, format!("Task failed: {}", msg));
-                    }
+            let gc = ocaml_runtime();
+            match res {
+                Ok(()) => {
+                    resolver.resolve(&gc, &());
                 }
-            });
-            let () = task.await;
+                Err(msg) => {
+                    resolver.reject(&gc, format!("Task failed: {}", msg));
+                }
+            }
         });
+        let () = task.await;
     });
-    task.detach();
-    fut
 }
