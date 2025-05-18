e85d209045d2c3d3462606815c33187f8e6f5b73
Konstantin A. Olkhovskiy <lupus@oxnull.net>
2025-05-18 19:05:32 +0400

    test: cover domain executor and promise

--- test-stubs/src/lib.rs
+++ test-stubs/src/lib.rs
@@ -69,6 +69,68 @@ pub fn lwti_tests_spawn_lwt(val: i64) -> ocaml_lwt_interop::promise::Promise<i64
     })
 }
 
+#[ocaml_lwt_interop::func]
+#[ocaml_gen::func]
+pub fn lwti_tests_run_in_ocaml_domain(f: OCamlFunc<(), ()>) -> () {
+    let handle = domain_executor::handle();
+    let join_handle = tokio::spawn(async move {
+        future::yield_now().await;
+        unsafe { run_in_ocaml_domain(&handle, move |gc| f.call(gc, ())) };
+    });
+    join_handle.await.unwrap();
+}
+
+#[ocaml_lwt_interop::func]
+#[ocaml_gen::func]
+pub fn lwti_tests_handle(f: OCamlAsyncFunc<(), ()>) -> () {
+    let handle = domain_executor::handle();
+    let join_handle = tokio::spawn(async move {
+        future::yield_now().await;
+        let task =
+            handle.spawn(async move { f.call(()).await.map_err(|e| e.to_string()) });
+        task.await.expect("ocaml task failed");
+    });
+    join_handle.await.unwrap();
+}
+
+#[ocaml_gen::func]
+#[ocaml::func]
+pub fn lwti_tests_promise_create(val: i64) -> ocaml_lwt_interop::promise::Promise<i64> {
+    use ocaml_lwt_interop::domain_executor::{ocaml_runtime, spawn_with_runtime};
+    let (promise, resolver) = ocaml_lwt_interop::promise::Promise::new(gc);
+    let task = spawn_with_runtime(gc, async move {
+        future::yield_now().await;
+        let gc = &ocaml_runtime();
+        resolver.resolve(gc, &val);
+    });
+    task.detach();
+    promise
+}
+
+#[ocaml_gen::func]
+#[ocaml::func]
+pub fn lwti_tests_promise_create_err(
+    msg: String,
+) -> ocaml_lwt_interop::promise::Promise<i64> {
+    use ocaml_lwt_interop::domain_executor::{ocaml_runtime, spawn_with_runtime};
+    let (promise, resolver) = ocaml_lwt_interop::promise::Promise::new(gc);
+    let task = spawn_with_runtime(gc, async move {
+        future::yield_now().await;
+        let gc = &ocaml_runtime();
+        resolver.reject(gc, msg);
+    });
+    task.detach();
+    promise
+}
+
+#[ocaml_lwt_interop::func]
+#[ocaml_gen::func]
+pub fn lwti_tests_await_promise(
+    p: ocaml_lwt_interop::promise::Promise<i64>,
+) -> Result<i64, String> {
+    p.await.map_err(|e| e.to_string())
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 //////////               OCaml bindings generation                   //////////
 ///////////////////////////////////////////////////////////////////////////////
@@ -80,5 +142,10 @@ ocaml_gen_bindings! {
         decl_func!(lwti_tests_test2 => "test_2");
         decl_func!(lwti_tests_test_sync_call => "test_sync_call");
         decl_func!(lwti_tests_spawn_lwt => "spawn_lwt");
+        decl_func!(lwti_tests_run_in_ocaml_domain => "run_in_ocaml_domain");
+        decl_func!(lwti_tests_handle => "handle_test");
+        decl_func!(lwti_tests_promise_create => "promise_create");
+        decl_func!(lwti_tests_promise_create_err => "promise_create_err");
+        decl_func!(lwti_tests_await_promise => "await_promise");
     });
 }
--- test/Stubs.ml
+++ test/Stubs.ml
@@ -4,4 +4,18 @@ module Tests = struct
   external test_2 : (unit -> unit Lwt.t) -> unit Lwt.t = "lwti_tests_test2"
   external test_sync_call : (unit -> unit) -> unit Lwt.t = "lwti_tests_test_sync_call"
   external spawn_lwt : int64 -> int64 Lwt.t = "lwti_tests_spawn_lwt"
+
+  external run_in_ocaml_domain
+    :  (unit -> unit)
+    -> unit Lwt.t
+    = "lwti_tests_run_in_ocaml_domain"
+
+  external handle_test : (unit -> unit Lwt.t) -> unit Lwt.t = "lwti_tests_handle"
+  external promise_create : int64 -> int64 Lwt.t = "lwti_tests_promise_create"
+  external promise_create_err : string -> int64 Lwt.t = "lwti_tests_promise_create_err"
+
+  external await_promise
+    :  int64 Lwt.t
+    -> (int64, string) result Lwt.t
+    = "lwti_tests_await_promise"
 end
